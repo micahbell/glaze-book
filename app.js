00383def909522ecf77c6a0668bcc23a8608fed5
Micah Bell <micahmbell@gmail.com>
2015-06-30 16:15:42 -0600

    Login and signup working.

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('dotenv').load();
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
-// var glazes = require('./routes/glazes');
 
 var app = express();
 
@@ -25,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-// app.use('/', glazes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,70 @@
 var express = require('express');
 var router = express.Router();
-var db = require('monk')('localhost/glaze-book');
+var db = require('monk')(process.env.MONGO);
 var userCollection = db.get('users');
 var bcrypt = require('bcryptjs');
-var salt = bcrypt.genSaltSync(10);
-var hash = bcrypt.hashSync("B4c0/\/", salt);
+var userValidation = require('../lib/user-validation');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/glazes', function (req, res, next) {
+router.get('/glazes', function(req, res, next) {
   res.render('glazes');
 });
 
-router.post('/signup', function (req, res, next) {
-  if(req.body.signup_password !== req.body.signup_confirm) {
-    res.render('index', { signupError: 'Passwords do not match.'});
-  } else {
-    var hash = bcrypt.hashSync(req.body.signup_password, 8);
-    userCollection.insert({
-      firstName: req.body.first_name,
-      lastName: req.body.last_name,
-      email: req.body.signup_email,
-      password: hash
-    });
-    res.redirect('/glazes');
-  };
-});
+router.post('/signup', function(req, res, next) {
+  var username = req.body.username.trim();
+  var email = req.body.signup_email.toLowerCase().trim();
+  var password = req.body.signup_password.trim();
+  var confirm = req.body.signup_confirm.trim();
+  var hash = bcrypt.hashSync(password, 8);
 
-router.post('/login', function (req, res, next) {
-  userCollection.findOne({ email: req.body.login_email }, function (err, user) {
-    if(user) {
-      var compare = bcrypt.compareSync(req.body.login_password, user.password);
-      if(compare === false) {
-        res.render('index', { loginError: 'Invalid login or password.'});
-      } else {
-        res.redirect('/glazes');
-      };
+  userValidation.existingUser(email, function(duplicateError) {
+    var validate = userValidation.signupValidation(username, email, password, confirm, duplicateError);
+    if(validate.length != 0) {
+      res.render('index', { signupError: validate, username: username, email: email });
     } else {
-      res.render('index', { loginError: 'Invalid login or password.'});
+        userCollection.insert({
+          username: username,
+          email: email,
+          password: hash
+        });
+        res.redirect('/glazes');
     };
   });
 });
+
+router.post('/login', function(req, res, next) {
+  var email = req.body.login_email.toLowerCase().trim();
+  var password = req.body.login_password.trim();
+
+  userValidation.loginUser(email, password, function(openSesame, user) {
+    console.log(user);
+    var validate = userValidation.loginValidation(email, password, openSesame);
+    if(validate.length != 0) {
+      res.render('index', { loginError: validate, email: email });
+    } else if (validate.length === 0) {
+      res.cookie('currentUser', user);
+      res.render('glazes', { currentUser: user});
+      res.redirect('/glazes');
+    };
+  });
+});
+
+
+//Can't set headers after they are sent?
+// router.post('/logout', function(req, res, next) {
+//   res.clearCookie('currentUser');
+//   res.redirect('/index');
+// });
+
+
+
+
+
+
+
 
 
 
